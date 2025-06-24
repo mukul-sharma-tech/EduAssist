03b5360a5ef7b51a4a110b1b257014c39b8ca83b
Mukul Sharma <muku0784@gmail.com>
2025-06-24 23:50:57 +0530

    Update SolutionUpload.jsx

--- Frontend/src/pages/AnswerChecker/SolutionUpload.jsx
+++ Frontend/src/pages/AnswerChecker/SolutionUpload.jsx
@@ -139,7 +139,7 @@ const SolutionUpload = () => {
 
     setIsLoading(true);
     try {
-      const res = await axios.post('http://localhost:5000/extract-text-pdf', formData, {
+      const res = await axios.post('https://eduassist-nak8.onrender.com/extract-text-pdf', formData, {
         headers: { 'Content-Type': 'multipart/form-data' },
       });
 
